8067bf72316e1c1a4fc703df53493ed2dec6e89a
Lucas Limeira <lucasalveslm@gmail.com>
2019-11-26 14:15:29 -0300

    fizzbuzz unit tests

---
+++ tests/unit/test_utils.py
@@ -0,0 +1,28 @@
+from random import randrange
+
+from fizzbuzz_core.utils import fizzbuzz
+
+
+def test_fizzbuzz():
+    multi_3 = (3 * randrange(-1000, 1000))
+    multi_5 = (5 * randrange(-1000, 1000))
+
+    multi_both = (multi_3 * multi_5)
+
+    assert fizzbuzz(multi_3) == 'fizz'
+    assert fizzbuzz(multi_5) == 'buzz'
+    assert fizzbuzz(multi_both) == 'fizzbuzz'
+
+
+def test_fizzbuzz_invalid_number():
+    test_cases = [
+        2, 7, 8, 11
+    ]
+
+    for number in test_cases:
+        assert fizzbuzz(number) == number
+
+
+def test_fizzbuzz_invalid_param():
+    assert fizzbuzz('not a number') == 'not a number'
+    assert fizzbuzz('') == ''
