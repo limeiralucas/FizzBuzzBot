9972a7cc45dd804ba9b17aef5e7340f46a6c16a4
Lucas Limeira <lucasalveslm@gmail.com>
2019-11-26 21:56:58 -0300

    getting access token

--- fizzbuzz_core/api/auth/controllers.py
+++ fizzbuzz_core/api/auth/controllers.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, request, current_app, redirect
 
-from fizzbuzz_core.utils.twitter import get_request_token, get_twitter_auth_url
+from fizzbuzz_core.data.models import db, Authentication
+from fizzbuzz_core.utils.twitter import get_request_token, get_twitter_auth_url, get_oauth_token, get_twitter_access_token
 
 auth = Blueprint('auth', __name__)
 
@@ -10,11 +11,40 @@ def login():
     request_token = get_request_token()
     url = get_twitter_auth_url(request_token)
 
+    new_auth = Authentication(
+        request_token['oauth_token'], request_token['oauth_token_secret'])
+    db.session.add(new_auth)
+    db.session.commit()
+
     return redirect(url)
 
 
 @auth.route('/twhook', methods=['GET'])
 def twitter_login_webhook():
-    print(request.args)
+    oauth_request_token = request.args.get('oauth_token')
+    oauth_verifier = request.args.get('oauth_verifier')
 
-    return jsonify({'status': 'ok'})
+    auth = Authentication.query.filter_by(
+        oauth_request_token=oauth_request_token).first()
+
+    if auth:
+        request_token = {
+            'oauth_token': oauth_request_token,
+            'oauth_token_secret': auth.oauth_request_token_secret,
+        }
+
+        token = get_oauth_token(request_token, oauth_verifier)
+
+        try:
+            access_token = get_twitter_access_token(token)
+            auth.oauth_token = access_token['oauth_token']
+            auth.oauth_token_secret = access_token['oauth_token_secret']
+            db.session.commit()
+
+            print(access_token)
+
+            return jsonify({'status': 'ok'})
+        except:
+            return jsonify({'status': 'error retrieving access_token'})
+
+    return jsonify({'status': 'error'})
--- fizzbuzz_core/data/models.py
+++ fizzbuzz_core/data/models.py
@@ -15,3 +15,18 @@ class Interaction(db.Model):
         self.sender = sender
         self.message = message
         self.response = response
+
+
+class Authentication(db.Model):
+    """ Model representing authentication proccess """
+    id = db.Column(db.Integer, primary_key=True)
+    oauth_request_token = db.Column(db.String(64))
+    oauth_request_token_secret = db.Column(db.String(64))
+    oauth_token = db.Column(db.String(64))
+    oauth_token_secret = db.Column(db.String(64))
+
+    def __init__(self, oauth_request_token, oauth_request_token_secret, oauth_token='', oauth_token_secret=''):
+        self.oauth_request_token = oauth_request_token
+        self.oauth_request_token_secret = oauth_request_token_secret
+        self.oauth_token = oauth_token
+        self.oauth_token_secret = oauth_token_secret
