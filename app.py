b21966ffb1843ab29b8e102fd022333e9dbc3f6f
Lucas Limeira <lucasalveslm@gmail.com>
2019-11-28 13:01:57 -0300

    fix app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from fizzbuzz_core import create_app, celery, init_celery
+from fizzbuzz_core import create_app
 
 if __name__ == '__main__':
     app = create_app()
-    init_celery(app, celery)
     app.run(host='0.0.0.0')
